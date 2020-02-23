0cab1d30204c3fad0beb48de4b7578676d96b571
shuntakeuch1 <takeuchishun89@gmail.com>
2020-02-23 21:37:03 +0900

    Brewfileの更新

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,4 @@ ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.emacs.d ~/.emacs.d
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/Brewfile ~/Brewfile
